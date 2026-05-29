ce5d141cd6f567c997f9d4392d488b28bcf11c9d
AlvarezSantiago-dev <santiagoalvarezc5@gmail.com>
2026-05-29 12:09:41 -0300

    chore: excluir scripts/ del repo (herramientas locales)

--- scripts/printbridge-v3/start.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "%~dp0"
-title CValle PrintBridge v3
-echo Iniciando CValle PrintBridge v3...
-echo Abre http://localhost:9100 para configurar.
-echo Deja esta ventana abierta (o minimizala).
-echo.
-node server.js
-pause
